fa428a1c4d6dc51a73b90694b27afa98d92c03bb
kadyroww <kadurovrasul668@gmail.com>
2023-11-27 15:52:48 +0600

    first commit

---
+++ lesson1.py
@@ -0,0 +1,35 @@
+# num1 = 12
+
+
+# # number = "13"
+# # print(type(int(number)))
+# # print(type(str(num1)))
+
+# # # if else elif
+
+# # if True:
+# #     print("Менин атым расул")
+# # else:
+# #     print("Менин атым расул эмес")
+
+
+# # nn = input("1:")
+# # nn2 = input("2:")
+# # if nn =nn2:
+# #     print("true")
+# # else:
+# #     print("Error")
+# #   
+login = input("Введите ваш логин: ")
+passwor = input("Введи те свой пароль: ")
+
+login_1 = "pov"
+passwor_1 = "qwerty"
+
+if login == login_1: 
+    if passwor == passwor_1:
+        print("добро пожаловать!!")
+    else:
+        print("у вас неправильный пароль введите еще раз")
+else:
+    print("у вас неправильный логин")
